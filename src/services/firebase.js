5e9047624a1eb176915a6d3e0b5d9ad9b50b8685
bsheenml <brendan.sheen@gmail.com>
2020-04-13 12:09:07 -0700

    Debug

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyC_KgNWvmyOEoZtskmCvoTS9r40lubHEKs",
-    authDomain: "chatty-app-demo.firebaseapp.com",
-    databaseURL: "https://chatty-app-demo.firebaseio.com",
-    projectId: "chatty-app-demo",
-    storageBucket: "chatty-app-demo.appspot.com",
-    messagingSenderId: "935394417001",
-    appId: "1:935394417001:web:18272569fbe28f547385f8",
-    measurementId: "G-6CESTEBDCW"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
 };
 firebase.initializeApp(config);
 export const auth = firebase.auth;
